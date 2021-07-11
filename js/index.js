42e011f3f3ed655e602e764fe39ddf568fe1bf8a
albonicc <nikygam@gmail.com>
2021-07-10 20:30:09 -0500

    Mark all Todo's work great

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
 let newTodo = document.querySelector('.newTodo');
 let listOfTodos = document.querySelector('.listOfTodos');
-let todoItems = document.querySelectorAll('.todo-item');
 
 const createTodo = () => {
     listOfTodos.innerHTML += `
-    <div class="todo-item" style="display: flex;">
-        <input type="checkbox" name="todo" style="margin-right: 15px;"/>
+    <div style="display: flex;">
+        <input type="checkbox" class="todo-item" name="todo" style="margin-right: 15px;"/>
         <p>${newTodo.value}</p>
     </div>    
     `
     newTodo.value = '';
+}
+
+const markAllTodos = () => {
+    let todoItems = document.querySelectorAll('.todo-item');
+    todoItems.forEach(todo => {
+        todo.setAttribute('checked', '');
+    })
 }
